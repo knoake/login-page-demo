452a67475b7ef5208c96e1ebaf1969b87a4e94dd
Katie Noake <katie@berrycloud.co.uk>
2024-01-31 10:51:53 +0000

    Add submit method to form and handle errors

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, map, shareReplay } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AuthService } from '../auth.service';
 
 export interface SignIn {
   username: string;
@@ -37,8 +38,46 @@ export class LoginPageComponent {
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    public router: Router
+    public router: Router,
+    private authService: AuthService
   ) {}
 
-  onSubmit(): void {}
+  get username() {
+    return this.signInForm.get('username');
+  }
+
+  get password() {
+    return this.signInForm.get('password');
+  }
+
+  onSubmit(): void {
+    this.submitting = true;
+
+    this.authService
+      .signIn(
+        this.signInForm.controls.username.value,
+        this.signInForm.controls.password.value
+      )
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/welcome']);
+        },
+        error: (error: Error) => {
+          this.submitting = false;
+          if (error.message.includes('expired')) {
+            this.signInForm.controls.password.setErrors({
+              accountExpired: true,
+            });
+          } else if (error.message === 'Password attempts exceeded') {
+            this.signInForm.controls.password.setErrors({
+              tooManyAttempts: true,
+            });
+          } else {
+            this.signInForm.controls.password.setErrors({
+              notAuthorized: true,
+            });
+          }
+        },
+      });
+  }
 }
